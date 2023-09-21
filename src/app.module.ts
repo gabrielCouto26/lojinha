f4eac4d764133e7ebdb365ed142e7ef9770c7d33
Gabriel Couto <gabrielcouto26@gmail.com>
2023-09-21 00:05:07 -0300

    remove service e controller de app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { User } from './users/user.entity';
@@ -19,7 +17,7 @@ import { User } from './users/user.entity';
     }),
     UsersModule
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
