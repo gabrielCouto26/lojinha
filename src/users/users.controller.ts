03b02d526368fc8576f2e56a539fdcb13a701d26
Gabriel Couto <gabrielcouto26@gmail.com>
2023-09-21 00:38:06 -0300

    implementa rota de criação de usuario

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -14,5 +15,10 @@ export class UsersController {
   @Get(':id')
   findOne(@Param(':id') id: string): Promise<User> {
     return this.usersService.findOne(+id);
+  }
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return this.usersService.create(createUserDto);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
+import { User as IUser } from './interface/user.interface';
 
 @Injectable()
 export class UsersService {
@@ -16,6 +17,10 @@ export class UsersService {
 
   findOne(id: number): Promise<User | null> {
     return this.usersRepository.findOneBy({ id });
+  }
+
+  create(user: IUser): Promise<User> {
+    return this.usersRepository.save(user);
   }
 
   async remove(id: number): Promise<void> {
